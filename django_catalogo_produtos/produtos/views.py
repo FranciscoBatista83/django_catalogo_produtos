d53a5ef6cbdc19b41de84cba7b5416500bea2c65
Franciscobatista83 <chicaobatista1983@gmail.com>
2025-06-29 20:51:49 -0300

    Criado formulário e view para cadastro de produtos via interface web

--- django_catalogo_produtos/produtos/views.py
+++ django_catalogo_produtos/produtos/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Produto
+from .forms import ProdutoForm
 
 def lista_produtos(request):
     produtos = Produto.objects.all()  # Busca todos os produtos no banco de dados
     return render(request, 'produtos/lista_produtos.html', {'produtos': produtos})
+
+def criar_produto(request):
+    if request.method == 'POST':
+        form = ProdutoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            
+            # Verifica qual botão foi clicado
+            if 'salvar_continuar' in request.POST:
+                # Redireciona para a própria página de cadastro (formulário limpo)
+                return redirect('criar_produto')
+            else:
+                # Redireciona para a lista de produtos
+                return redirect('lista_produtos')
+    else:
+        form = ProdutoForm()
+    
+    return render(request, 'produtos/criar_produto.html', {'form': form})
